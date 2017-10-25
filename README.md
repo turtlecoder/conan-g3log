3d230f8c4bc7cfd91f5089f27355eeb1595a84e0
Oskar Gargas <oskar.gargas@hpsupport.pl>
2017-10-25 16:00:23 +0200

    Updates to support current g3log version and macOS

--- README.md
+++ README.md
@@ -1,22 +1,4 @@
 # conan-g3log
-Conan.IO package for g3logger
+Conan.io package for g3logger
 
-[[G3Log]](https://github.com/KjellKod/g3log) packaged for [[Conan]](https://github.com/conan-io/conan)
-
-Conan repo: https://conan.io/source/g3log/master/Brunni/testing
-
-## Current state
-
-* Windows Visual Studio 14 working
-
-## TODO
-
-* Building as shared lib
-* gtest necessary?
-* does debug work?
-
-## Testing
-
-```bash
-$ conan test_package
-```
+[G3Log](https://github.com/KjellKod/g3log) packaged for [Conan](https://github.com/conan-io/conan)
--- conanfile.py
+++ conanfile.py
@@ -1,60 +1,66 @@
 from conans import ConanFile, CMake, tools
 import os
 
-class G3logConan(ConanFile):
-	name = "g3log"
-	version = "master"
-	license = "Unlicense"
-	url = "https://github.com/Brunni/conan-g3log"
-	description = "G3log is an asynchronous, crash safe, logger that is easy to use with default logging sinks or you can add your own."
-	settings = "os", "compiler", "build_type", "arch"
-	options = {"shared": [True, False]}
-	default_options = "shared=False"
-	generators = "cmake"
-	
-	def config(self):
-		if self.scope.dev and self.scope.build_tests:
-			self.requires( "gtest/1.8.0@lasote/stable" )
-			self.options["gtest"].shared = False
-		print("Description is: %s" % self.description)
-		print("default_option is: %s" % self.default_options)
-		print("shared is: %s" % self.options.shared)
 
-	def source(self):
-		self.run("git clone --depth 1 https://github.com/KjellKod/g3log.git")
-		#self.run("cd hello && git checkout static_shared")
-		# This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
-		# if the packaged project doesn't have variables to set it properly
-		tools.replace_in_file("g3log/CMakeLists.txt", "project (g3log)", '''project (g3log)
+class G3logConan(ConanFile):
+    name = "g3log"
+    version = "master"
+    license = "MIT"
+    url = "https://github.com/oskargargas/conan-g3log"
+    source_url = "https://github.com/KjellKod/g3log"
+    commit = "master"
+    description = "G3log is an asynchronous, crash safe, logger that is easy to use with default logging sinks or you can add your own."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def config(self):
+        if self.scope.dev and self.scope.build_tests:
+            self.requires("gtest/1.8.0@lasote/stable")
+            self.options["gtest"].shared = False
+
+    def source(self):
+        if not os.path.isdir('g3log'):
+            self.run('git clone %s g3log' % self.source_url)
+        else:
+            self.run('cd g3log && git fetch origin')
+
+        self.run('cd g3log && git checkout %s' % self.commit)
+
+        tools.replace_in_file("g3log/CMakeLists.txt", "project (g3log)", '''project (g3log)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
 
-	def build(self):
-		cmake = CMake(self.settings)
-		cmake_opts = "-DUSE_CONAN=True "
-		cmake_opts += "-DADD_G3LOG_UNIT_TEST=True " if self.scope.dev and self.scope.build_tests else ""
-		if self.options.shared:
-			print("Building shared lib only")
-			target = "g3logger_shared"
-			cmake_opts += "-DADD_BUILD_WIN_SHARED=True "
-		else:
-			print("Building static lib only")
-			target = "g3logger"
-		
-		cmake_opts += "-DADD_FATAL_EXAMPLE=OFF "
-		self.run('cmake %s/g3log %s %s' % (self.conanfile_directory, cmake.command_line, cmake_opts))
-		# We need to prevent to build static library as well when building shared. It might overwrite the lib file!
-		self.run("cmake --build . %s --target %s" % (cmake.build_config, target))
+    def build(self):
+        cmake = CMake(self.settings)
 
-	def package(self):
-		self.copy("*.hpp", dst="include", src="g3log/src")
-		self.copy("*.lib", dst="lib", keep_path=False)
-		self.copy("*.exp", dst="lib", keep_path=False)
-		self.copy("*.dll", dst="bin", keep_path=False) #shared lib
-		self.copy("*.so", dst="lib", keep_path=False)
-		self.copy("*.a", dst="lib", keep_path=False)
+        cmake_opts = "-DADD_G3LOG_UNIT_TEST=True " if self.scope.dev and self.scope.build_tests else ""
+        cmake_opts += "-DBUILD_EXAMPLES:BOOL=False "
+        cmake_opts += "-DADD_FATAL_EXAMPLE=OFF "
+        if self.settings.os == "Macos":
+            cmake_opts += "-DCMAKE_SKIP_RPATH:BOOL=ON "
 
-	def package_info(self):
-		self.cpp_info.libs = ["g3logger"]
-		if self.settings.os == "Linux":
-			self.cpp_info.libs.append("pthread")
+        if self.options.shared:
+            target = "g3logger_shared"
+            cmake_opts += "-DADD_BUILD_WIN_SHARED=True "
+        else:
+            target = "g3logger"
+
+        self.run('cmake "%s/g3log" %s %s' % (self.conanfile_directory, cmake.command_line, cmake_opts))
+        self.run('cmake --build . %s --target %s' % (cmake.build_config, target))
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src="g3log/src")
+        self.copy("*.hpp", dst="include", src="include")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.exp", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["g3logger"]
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
